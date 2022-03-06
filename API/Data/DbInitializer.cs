34c5ea4ee03e7fcf46e6299dfeafa7800b610aec
CiprianP99 <ciprianplaisanu@gmail.com>
2022-03-06 18:27:13 +0200

    Commit 2

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -18,50 +18,50 @@ namespace API.Data
             {
                 new Product
                 {
-                    Name = "RTX 2080 Gigabyte 12 GB",
-                    Description="sadasdj",
-                    Price = 2000,
-                    PictureUrl = "/images/products/rtx2080gigabyte",
+                    Name = "GIGABYTE GeForceRTX 2080 Super Windforce 8G",
+                    Description="Powered by GeForce RTX 2080 SUPER. Nvidia Turing architecture & Real time Ray Tracing",
+                    Price = 120000,
+                    PictureUrl = "/images/products/rtx2080gigabyte.jpg",
                     Type = "Graphics Card",
                     Brand = "Nvidia",
                     QuantityInStock = 20
                 },
                 new Product
                 {
-                    Name = "Intel Core i7 9770k",
-                    Description="sadasdjsdadsada",
-                    Price = 500,
-                    PictureUrl = "/images/products/i79770k",
+                    Name = "Intel Core i7-9700K Desktop Processor",
+                    Description="8 Cores / 8 Threads. 3.60 GHz up to 4.90 GHz / 12 MB Cache.Compatible only with Motherboards based on Intel 300 Series Chipsets",
+                    Price = 50000,
+                    PictureUrl = "/images/products/i79700k.jpg",
                     Type = "Processor",
                     Brand = "Intel",
                     QuantityInStock = 15
                 },
                 new Product
                 {
-                    Name = "Radeon Rx580",
-                    Description="sadasdj",
-                    Price = 900,
-                    PictureUrl = "/images/products/rx580",
+                    Name = "Sapphire Pulse Radeon RX 580",
+                    Description="DirectX 12 11265-05-20T 8GB 256-Bit GDDR5 PCI Express 3.0 x16 CrossFireX Support ATX Video Card",
+                    Price = 90000,
+                    PictureUrl = "/images/products/rx580.jpg",
                     Type = "Graphics Card",
                     Brand = "AMD",
                     QuantityInStock = 20
                 },
                 new Product
                 {
-                    Name = "SSD 500gb",
-                    Description="sadasdj",
-                    Price = 150,
-                    PictureUrl = "/images/products/rtx2080gigabyte",
+                    Name = "Intel SSD 3.84TB 3D NAND",
+                    Description="Form Factor: 2.5 inch. Random Read (100% span): 97, 000 IOPS",
+                    Price = 75599,
+                    PictureUrl = "/images/products/intelssd.jpg",
                     Type = "Memory",
                     Brand = "Intel",
                     QuantityInStock = 10
                 },
                 new Product
                 {
-                    Name = "Ryzen 5",
-                    Description="sadasdj",
-                    Price = 300,
-                    PictureUrl = "/images/products/rryzen5",
+                    Name = "AMD Ryzen 5 5600X 6-core",
+                    Description="AMD's fastest 6 core processor for mainstream desktop, with 12 processing threads",
+                    Price = 29999,
+                    PictureUrl = "/images/products/ryzen5600x.jpg",
                     Type = "Processor",
                     Brand = "AMD",
                     QuantityInStock = 45
